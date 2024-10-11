5140e3ac5b003785153e34fb85524a175d5fb66e
LucasJesuino <133780251+LucasJesuino@users.noreply.github.com>
2024-10-11 11:05:32 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,65 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+
+)
+criaCartao(
+     'Filosofia',
+    'De quem é a famosa frase “Penso, logo existo”?',
+    ' Descartes'
+)
+criaCartao(
+    'história',
+   'Qual o nome do presidente do Brasil que ficou conhecido como Jango?”?',
+   ' João Goulart.'
+)
+
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+
+)
+criaCartao(
+     'Filosofia',
+    'De quem é a famosa frase “Penso, logo existo”?',
+    ' Descartes'
+)
+criaCartao(
+    'história',
+   'Qual o nome do presidente do Brasil que ficou conhecido como Jango?”?',
+   ' João Goulart.'
+)
+
